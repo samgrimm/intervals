5eb98126e41bb3e9ed74d4c619bce78445b9b210
samgrimm <samantha.grimm@gmail.com>
2016-01-21 15:53:32 -0600

    Initial Commit

---
+++ intervals/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  intervals
+//
+//  Created by Samantha Cabral on 1/21/16.
+//  Copyright © 2016 Samantha Cabral. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ intervals/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  intervals
+//
+//  Created by Samantha Cabral on 1/21/16.
+//  Copyright © 2016 Samantha Cabral. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
